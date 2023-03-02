474e6a3f57009633ddcf80668177f8088f6dc6e7
k5md <serg.dfc@yandex.ru>
2023-03-02 19:31:06 +0300

    prepare to refactor overlay by separating features to be reused (overlay, draggable, resizable)

---
+++ src/hide_it/libs/OverlayWindow.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+from hide_it.utils import enable_clickthrough, disable_clickthrough
+
+class OverlayWindow(tk.Toplevel):
+    def __init__(self, *args, **kwargs):
+        tk.Toplevel.__init__(self, *args, **kwargs)
+
+        # CONFIGURE WIDGETS
+        self.overrideredirect(True)
+        self.attributes("-topmost", True)
+        
+    def lock(self):
+        enable_clickthrough(self.winfo_id())
+        self.attributes("-transparentcolor", self.overlay_color)
+
+    def unlock(self):
+        disable_clickthrough(self.winfo_id())
+        self.attributes("-transparentcolor", "")
+        self.attributes("-topmost", True)
--- src/hide_it/utils.py
+++ src/hide_it/utils.py
@@ -4,8 +4,10 @@ import win32api
 import win32gui
 import win32con
 from PIL import ImageColor
+from typing import Union
+import tkinter as tk
 
-def apply_to_widget_and_children(widget, fn):
+def apply_to_widget_and_children(widget: tk.Widget, fn):
     fn(widget)
     for child in widget.winfo_children():
         apply_to_widget_and_children(child, fn)            
@@ -33,3 +35,40 @@ def enable_clickthrough(hwnd):
 
 def disable_clickthrough(hwnd):
     win32api.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, 0)
+
+def make_draggable(widget: tk.Widget, grip: tk.Widget):
+    press_x = None
+    press_y = None
+
+    def handle_press(event):
+        press_x = event.x
+        press_y = event.y
+
+    def handle_drag(event):
+        dx = event.x - press_x
+        dy = event.y - press_y
+        x = widget.winfo_x() + dx
+        y = widget.winfo_y() + dy
+        widget.geometry(f"+{x}+{y}")
+
+    grip.bind("<Button-1>", handle_press)
+    grip.bind("<B1-Motion>", handle_drag)
+
+def make_resizable(widget: tk.Widget, grip: tk.Widget, side: Union[tk.SE, tk.NW, tk.NE, tk.SW]):
+    def resize(event):
+        min_w = widget.minsize()[0]
+        min_h = widget.minsize()[1]
+        (x0, y0, x1, y1) = (widget.winfo_rootx(), widget.winfo_rooty(), widget.winfo_pointerx(), widget.winfo_pointery())
+        # offsets from mouse to window
+        dx = x1 - x0 
+        dy = y1 - y0
+        if side == tk.SE: (x, y, w, h) = (x0, y0, dx, dy)
+        elif side == tk.NW: (x, y, w, h) = (x1, y1, widget.winfo_width() - dx, widget.winfo_height() - dy)
+        elif side == tk.NE: (x, y, w, h) = (x1 - dx, y1, dx, widget.winfo_height() - dy)
+        elif side == tk.SW: (x, y, w, h) = (x1, y1 - dy, -dx + widget.winfo_width(), dy)
+        # prevent window from moving
+        if (w < min_w): (x, w) = (x0, min_w)
+        if (h < min_h): (y, h) = (y0, min_h)
+        widget.geometry(f"{w}x{h}+{x}+{y}")
+
+    grip.bind("<B1-Motion>", resize)
